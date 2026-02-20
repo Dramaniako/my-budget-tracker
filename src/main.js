d8468551ab30cf38c541a87cf8ff18d7d5109766
Dramaniako <wipradnyanabayu@gmail.com>
2026-02-20 22:23:46 +0800

    initializing project

---
+++ src/main.js
@@ -0,0 +1,22 @@
+const { app, BrowserWindow } = require('electron');
+const path = require('path');
+
+function createWindow() {
+    const win = new BrowserWindow({
+        width: 1000,
+        height: 800,
+        webPreferences: {
+            preload: path.join(__dirname, 'preload.js'), // Securely connects UI to Node
+            nodeIntegration: false, // Security best practice
+            contextIsolation: true
+        }
+    });
+
+    win.loadFile('src/ui/index.html');
+}
+
+app.whenReady().then(createWindow);
+
+app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') app.quit();
+});
